af7ce874cde8f6c2de666840e8ae84fa39552d2b
Edilson <edilson.a.andrade@gmail.com>
2021-08-30 19:44:56 -0300

    creating documnentation for challenge 2 with swagger

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import express from "express";
-
+import swaggerUi from "swagger-ui-express";
+import swaggerFile from "../swagger-confing.json";
 import { usersRoutes } from "./routes/users.routes";
 
 const app = express();
 
 app.use(express.json());
-
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/users", usersRoutes);
 
 export { app };
